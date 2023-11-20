88f76d9771449ed60a36a98e12ccb6f5e3bde862
youngil <youngil@placeb.com>
2023-11-20 18:28:20 +0900

    멤버함수

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -28,25 +28,23 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// Move platform forwards
-		// Get current location
-	FVector CurrentLocation = GetActorLocation();
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
 
-		// Add vector to that location
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	FVector CurrentLocation = GetActorLocation();
 	CurrentLocation = CurrentLocation + PlatformVelocity * DeltaTime;
 
-		// Set the location
 	SetActorLocation(CurrentLocation);
 
-	// Send platform back if gone too far
-		// Check how far we`ve moved
 	float DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
-		// Reverse direction of motion if gone too far
 	if (DistanceMoved > MoveDistance)
 	{
 		float OverShot = DistanceMoved - MoveDistance;
 		FString Name = GetName();
-		 
+
 		UE_LOG(LogTemp, Display, TEXT("%s Platform Overshot by %f"), *Name, OverShot);
 
 		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
@@ -54,6 +52,11 @@ void AMovingPlatform::Tick(float DeltaTime)
 		SetActorLocation(StartLocation);
 		PlatformVelocity = -PlatformVelocity;
 	}
-
 }
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	UE_LOG(LogTemp, Display, TEXT("%s Rotating..."), *GetName());
+}
+
 
